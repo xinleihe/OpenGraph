105a60857b3e112718431c6a376e8c19fd924877
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2019-12-04 17:58:01 +0800

    1. Added evaluation for NetworkX

---
+++ Time Consuming Evaluation/NetworkX_trial.py
@@ -0,0 +1,91 @@
+import networkx as nx
+from networkx.algorithms import approximation
+from TimeRecord import TimeRecord
+
+
+def generate_graph_erdos_renyi(n=100, p=0.2, directed=None):
+    g = nx.generators.random_graphs.erdos_renyi_graph(
+        n=n, p=p, directed=directed)
+
+
+def generate_complete_graph(n=100, create_using=None):
+    g = nx.generators.classic.complete_graph(n=n, create_using=create_using)
+
+
+def generate_directed_gn_graph(n=100, kernel=None):
+    g = nx.generators.directed.gn_graph(n=n, kernel=kernel)
+
+
+def get_subgraph(graph, nodes=[(i+1) for i in range(20)]):
+    sg = graph.subgraph(nodes)
+
+
+def get_connected_components_number(graph: nx.Graph):
+    connected_components_number = nx.algorithms.components.number_connected_components(
+        G=graph)
+
+
+def get_strongly_connected_components_number(graph: nx.DiGraph):
+    strongly_connected_components_number = nx.algorithms.components.number_strongly_connected_components(
+        G=graph)
+
+
+def get_weakly_connected_components_number(graph: nx.DiGraph):
+    weakly_connected_components_number = nx.algorithms.components.number_weakly_connected_components(
+        G=graph)
+
+
+def get_all_cliques(graph: nx.Graph):
+    cliques = nx.algorithms.clique.enumerate_all_cliques(G=graph)
+
+
+def get_min_edge_dominating_set(graph: nx.Graph):
+    meds = nx.algorithms.approximation.dominating_set.min_edge_dominating_set(
+        G=graph)
+
+
+def get_closeness_centrality(graph):
+    cc = nx.algorithms.centrality.closeness_centrality(G=graph)
+
+
+def get_betweenness_centrality(graph):
+    bc = nx.algorithms.centrality.betweenness_centrality(G=graph)
+
+
+def get_communities_girvan_newman(graph):
+    communities = nx.algorithms.community.centrality.girvan_newman(G=graph)
+
+
+def get_clustering_coefficient(graph):
+    cc = nx.algorithms.cluster.average_clustering(G=graph)
+
+
+def get_k_core(graph, K=5):
+    kc = nx.algorithms.core.k_core(G=graph, k=K)
+
+
+if __name__ == "__main__":
+    recorder = TimeRecord(package_name='NetworkX')
+
+    recorder.time_record(func=generate_graph_erdos_renyi, n=100, p=0.2)
+    recorder.time_record(func=generate_complete_graph, n=100)
+    recorder.time_record(func=generate_directed_gn_graph, n=100)
+
+    recorder.set_graph_generator_config(directed=False, nodes_num=100, erdos_renyi_probability=0.2)
+    recorder.time_record_with_graph(func=get_subgraph)
+
+    recorder.set_graph_generator_config(directed=False, nodes_num=100, erdos_renyi_probability=0.05)
+    recorder.time_record_with_graph(func=get_connected_components_number)
+
+    recorder.set_graph_generator_config(directed=True, nodes_num=100, erdos_renyi_probability=0.05)
+    recorder.time_record_with_graph(func=get_strongly_connected_components_number)
+    recorder.time_record_with_graph(func=get_weakly_connected_components_number)
+
+    recorder.set_graph_generator_config(directed=False, nodes_num=100, erdos_renyi_probability=0.2)
+    recorder.time_record_with_graph(func=get_all_cliques, test_counts=10)
+    recorder.time_record_with_graph(func=get_min_edge_dominating_set)
+    recorder.time_record_with_graph(func=get_closeness_centrality, test_counts=100)
+    recorder.time_record_with_graph(func=get_betweenness_centrality, test_counts=100)
+    recorder.time_record_with_graph(func=get_communities_girvan_newman, test_counts=100)
+    recorder.time_record_with_graph(func=get_clustering_coefficient)
+    recorder.time_record_with_graph(func=get_k_core, K=5)
