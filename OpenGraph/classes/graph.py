94113b3ddf04b4dab344a1d1954b10e335384ac8
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2019-12-16 03:19:05 +0800

    1. Added functions for removing nodes and edges; 2. Added hyper property __len__ and __iter__

--- OpenGraph/classes/graph.py
+++ OpenGraph/classes/graph.py
@@ -17,6 +17,17 @@ class Graph(object):
 
         self.graph.update(graph_attr)
 
+    def __iter__(self):
+        return iter(self._node)
+    
+    def __len__(self):
+        return len(self._node)
+
+    @property
+    def adj(self):
+        # TODO: AdjView of the graph. See networkx.
+        return list(self._adj.keys())
+
     def add_node(self, node_for_adding, **node_attr):
         self._add_one_node(node_for_adding, node_attr)
 
@@ -50,16 +61,18 @@ class Graph(object):
         self._add_one_edge(u_of_edge, v_of_edge, edge_attr={"weight": weight})
 
     def add_edges(self, edges_for_adding, edges_attr: [dict] = []):
-        if not len(edges_attr) == 0: # Edges attributes included in input
-            assert len(edges_for_adding) == len(edges_attr), "Edges and Attributes lists must have same length."
-        else: # Set empty attribute for each edge
+        if not len(edges_attr) == 0:  # Edges attributes included in input
+            assert len(edges_for_adding) == len(
+                edges_attr), "Edges and Attributes lists must have same length."
+        else:  # Set empty attribute for each edge
             edges_attr = [dict() for i in range(len(edges_for_adding))]
 
         for i in range(len(edges_for_adding)):
             try:
                 edge = edges_for_adding[i]
                 attr = edges_attr[i]
-                assert len(edge) == 2, "Edge tuple {} must be 2-tuple.".format(edge)
+                assert len(
+                    edge) == 2, "Edge tuple {} must be 2-tuple.".format(edge)
                 self._add_one_edge(edge[0], edge[1], attr)
             except Exception as err:
                 print(err)
@@ -79,4 +92,33 @@ class Graph(object):
         self._adj[u][v] = datadict
         self._adj[v][u] = datadict
 
+    def remove_node(self, node_to_remove):
+        try:
+            neighbors = list(self._adj[node_to_remove])
+            del self._node[node_to_remove]
+        except KeyError:  # Node not exists in self
+            raise KeyError("No node {} in graph.".format(node_to_remove))
+        for neighbor in neighbors:  # Remove edges with other nodes
+            del self._adj[neighbor][node_to_remove]
+        del self._adj[node_to_remove]  # Remove this node
+
+    def remove_nodes(self, nodes_to_remove: list):
+        for node in nodes_to_remove:  # If not all nodes included in graph, give up removing other nodes
+            assert (node in self._node), "Remove Error: No node {} in graph".format(
+                node)
+        for node in nodes_to_remove:
+            self.remove_node(node)
+
+    def remove_edge(self, u, v):
+        try:
+            del self._adj[u][v]
+            if u != v: # self-loop needs only one entry removed
+                del self._adj[v][u]
+        except KeyError:
+            raise KeyError("No edge {}-{} in graph.".format(u, v))
     
+    def remove_edges(self, edges_to_remove: [tuple]):
+        for edge in edges_to_remove:
+            u, v = edge[:2]
+            self.remove_edge(u, v)
+            
