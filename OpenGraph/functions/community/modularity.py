639a9289631d051e642f74883c9f256d7d214348
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2020-02-02 19:12:52 +0800

    Updated modularity computation

--- OpenGraph/functions/community/modularity.py
+++ OpenGraph/functions/community/modularity.py
@@ -5,57 +5,12 @@ __all__ = [
 ]
 
 def modularity(G, communities, weight='weight'):
-    r"""Returns the modularity of the given partition of the graph.
-
-    Modularity is defined in [1]_ as
-
-    .. math::
-
-        Q = \frac{1}{2m} \sum_{ij} \left( A_{ij} - \frac{k_ik_j}{2m}\right)
-            \delta(c_i,c_j)
-
-    where $m$ is the number of edges, $A$ is the adjacency matrix of
-    `G`, $k_i$ is the degree of $i$ and $\delta(c_i, c_j)$
-    is 1 if $i$ and $j$ are in the same community and 0 otherwise.
-
-    Parameters
-    ----------
-    G : NetworkX Graph
-
-    communities : list or iterable of set of nodes
-        These node sets must represent a partition of G's nodes.
-
-    Returns
-    -------
-    Q : float
-        The modularity of the paritition.
-
-    Raises
-    ------
-    NotAPartition
-        If `communities` is not a partition of the nodes of `G`.
-
-    Examples
-    --------
-    >>> import networkx.algorithms.community as nx_comm
-    >>> G = nx.barbell_graph(3, 0)
-    >>> nx_comm.modularity(G, [{0, 1, 2}, {3, 4, 5}])
-    0.35714285714285704
-    >>> nx_comm.modularity(G, nx_comm.label_propagation_communities(G))
-    0.35714285714285704
-
-    References
-    ----------
-    .. [1] M. E. J. Newman *Networks: An Introduction*, page 224.
-       Oxford University Press, 2011.
-
-    """
     # TODO: multigraph and directed detection not included. See networkx.
 
     if not isinstance(communities, list):
         communities = list(communities)
 
-    m = 1
+    m = G.size
     out_degree = dict(G.degree)
     in_degree = out_degree
     norm = 1 / (2 * m)
@@ -72,3 +27,18 @@ def modularity(G, communities, weight='weight'):
 
     Q = sum(val(u, v) for c in communities for u, v in product(c, repeat=2))
     return Q * norm
+
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append('../../../')
+    import OpenGraph as og
+    
+    g = og.Graph()
+    edges1 = [(1, 2), (2, 3), (1, 3), (3, 4), (4, 5), (4, 6), (5, 6)]
+    g.add_edges(edges1)
+
+    cmnts = [frozenset([1, 2, 3]), frozenset([4, 5, 6])]
+    print(modularity(G=g, communities=cmnts))
+
+
