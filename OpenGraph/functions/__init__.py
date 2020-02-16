4e5fcaca471da9fcce141b617ba820ec96c41f2c
ICYPOLE <39186199+ICYPOLE@users.noreply.github.com>
2020-02-16 18:21:08 +0800

    Updated __init__.py

--- OpenGraph/functions/__init__.py
+++ OpenGraph/functions/__init__.py
@@ -1,6 +1,7 @@
 from OpenGraph.functions.components import *
 from OpenGraph.functions.community import *
 from OpenGraph.functions.structural_holes import *
+from OpenGraph.functions.graph_embedding import *
 
 from OpenGraph.functions.not_sorted import *
 # import OpenGraph.functions.components
